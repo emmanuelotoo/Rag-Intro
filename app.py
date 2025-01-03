d21d24cbd0424d547840269de8a03ea7c23359e6
emmanuelotoo <167821127+emmanuelotoo@users.noreply.github.com>
2025-01-03 18:00:10 +0000

    second commit

--- app.py
+++ app.py
@@ -8,13 +8,30 @@ from chromadb.utils import embedding_functions
 load_dotenv()
 
 # Initialize OpenAI API
-openai_key = os.getenv("API_KEY")
+openai_key = os.getenv("OPENAI_API_KEY")
 
-openai_ef = embedding_functions.OpenAIEmbeddingFunctions(
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
     api_key=openai_key, model_name="text-embedding-3-small"
 )
 
 # initialize the Chroma client with persistence
 chroma_client = chromadb.PersistentClient(path="chroma_persistent_storage")
 collection_name = "document_qa_collection"
+collection = chroma_client.get_or_create_collection(
+    name=collection_name, embedding_function=openai_ef
+)
 
+client = OpenAI(api_key=openai_key)
+
+resp = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "What is human life expectancy in the United States?",
+        },
+    ],
+)
+
+print(resp)
