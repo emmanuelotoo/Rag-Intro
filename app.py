993a58ab25fe98cddbd59cb846f6ffe03e11f104
emmanuelotoo <167821127+emmanuelotoo@users.noreply.github.com>
2025-03-05 02:51:16 +0000

    changes

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import chromadb
 from openai import OpenAI
 from chromadb.utils import embedding_functions
 
-# Load environment variables from .env file
+
 load_dotenv()
 
-# Initialize OpenAI API
+
 openai_key = os.getenv("OPENAI_API_KEY")
 
 openai_ef = embedding_functions.OpenAIEmbeddingFunction(
     api_key=openai_key, model_name="text-embedding-3-small"
 )
 
-# initialize the Chroma client with persistence
+
 chroma_client = chromadb.PersistentClient(path="chroma_persistent_storage")
 collection_name = "document_qa_collection"
 collection = chroma_client.get_or_create_collection(
